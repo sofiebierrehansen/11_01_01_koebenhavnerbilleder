66509ead638af00fd1f4d5f22d7d2498daea9649
Sofie Bierre Hansen <sofi02a7@stud.kea.dk>
2022-08-25 15:52:35 +0200

    ændret farvet

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      cloudy: "#CFEAEB",
+      gloomy: "#6DA4A3",
+      army: "#6DA4A3",
+      light: "#FAFAFA",
+      dark: "#262626",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
